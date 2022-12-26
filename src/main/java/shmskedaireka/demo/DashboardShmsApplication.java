b0d200dd22d36466f4b2bb27ff0d8bc04b811e57
luthfi070 <luthfimusafa@gmail.com>
2022-12-26 21:51:33 +0700

    fix url

--- src/main/java/shmskedaireka/demo/DashboardShmsApplication.java
+++ src/main/java/shmskedaireka/demo/DashboardShmsApplication.java
@@ -34,14 +34,14 @@ public class DashboardShmsApplication {
 		return "hello";
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
-	}
+	// @Bean
+	// public WebMvcConfigurer corsConfigurer() {
+	// return new WebMvcConfigurer() {
+	// @Override
+	// public void addCorsMappings(CorsRegistry registry) {
+	// registry.addMapping("/**").allowedOrigins("*");
+	// }
+	// };
+	// }
 
 }
